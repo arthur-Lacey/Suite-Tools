72545fd60e8bbf35ac09f273c56f0942f5f91ca0
Arthur Lacey <arthur.lacey@usma.edu>
2016-02-28 00:01:00 -0500

    Parallel Processing and bit-string generation
    Added functionality for the parallel processing of all possible messages
    through the calling of the parseBitString function. Also added a
    function to convert a normal ascii string to it's binary representation.

--- string decoder.py
+++ string decoder.py
@@ -7,9 +7,13 @@ e.g.
 >>> parseBitString(a,0)
 'ACD'
 '''
+from multiprocessing import *
+
+bitsInAByte=8 #this is a constant value
+
 def parseBitString(string, startIndex):
     message="" #stores the decoded message
-    bitsInAByte=8 #this is a constant value
+    
     i=startIndex #i represents the beginning of the substring to examine
     while(i<len(string)):#if i is greater, there will be no substring
         substring=string[i:i+bitsInAByte]
@@ -18,3 +22,30 @@ def parseBitString(string, startIndex):
         i+=bitsInAByte
     return message
             
+def parseAllStrings(string):
+	pool=Pool(processes=bitsInAByte)
+	messages= [None]*bitsInAByte
+
+	
+	for i in range(bitsInAByte):
+		messages[i]=pool.apply_async(parseBitString,[string,i])
+
+	for i in range(len(messages)):
+		messages[i]=messages[i].get()
+	
+	#pool.map(parseBitString,string,range(bitsInAByte))
+	
+	#return pool
+	return messages
+
+def generateBitString(string): #used for testing purposes
+	output=""
+	for letter in string:
+		binaryString=bin(ord(letter))[2:]
+		while(len(binaryString)<8):
+			binaryString="0"+binaryString
+		output=output+binaryString
+	return output
+
+
+
